ff1f47ae3faa141f096f17dd0affc7a8a6c50ec5
Alex <alexhart@gmail.com>
2022-12-13 22:59:27 -0800

    adding resources and folders in pybank main adding commenting for direction

---
+++ PyBank/main.py
@@ -0,0 +1,23 @@
+import os
+import csv
+
+# bring in csv into script
+
+# read file
+
+# find header rows
+
+# get total count of list items in first column
+
+# absolute value of all changes
+
+# average change (sum of real values / number of items)
+
+# find max 
+
+# find min
+
+# print analysis to terminal
+
+#write analysis to new text file
+
